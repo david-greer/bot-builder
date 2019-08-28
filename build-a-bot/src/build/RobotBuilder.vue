353f08635f429f9bd38710eb436fd6afc949f7c1
David Greer <greerdavid4411@gmail.com>
2019-08-28 11:05:31 -0700

    Bind to the data

--- build-a-bot/src/build/RobotBuilder.vue
+++ build-a-bot/src/build/RobotBuilder.vue
@@ -2,41 +2,48 @@
   <div>
     <div class="top-row">
       <div class="top part">
-        <img src="./images/head-big-eye.png" title="head"/>
-        <button class="prev-selector">&#9668;</button>
-        <button class="next-selector">&#9658;</button>
+        <img v-bind:src="availableParts.heads[0].src" title="head"/>
+        <button v-on:click="" class="prev-selector">&#9668;</button>
+        <button v-on:click="" class="next-selector">&#9658;</button>
       </div>
     </div>
     <div class="middle-row">
       <div class="left part">
-        <img src="./images/arm-articulated-claw.png" title="left arm"/>
-        <button class="prev-selector">&#9650;</button>
-        <button class="next-selector">&#9660;</button>
+        <img v-bind:src="availableParts.arms[0].src" title="left arm"/>
+        <button v-on:click="" class="prev-selector">&#9650;</button>
+        <button v-on:click="" class="next-selector">&#9660;</button>
       </div>
       <div class="center part">
-        <img src="./images/torso-flexible-gauged.png" title="left arm"/>
-        <button class="prev-selector">&#9668;</button>
-        <button class="next-selector">&#9658;</button>
+        <img v-bind:src="availableParts.torsos[0].src" title="torso"/>
+        <button v-on:click="" class="prev-selector">&#9668;</button>
+        <button v-on:click="" class="next-selector">&#9658;</button>
       </div>
       <div class="right part">
-        <img src="./images/arm-dual-claw.png" title="left arm"/>
-        <button class="prev-selector">&#9650;</button>
-        <button class="next-selector">&#9660;</button>
+        <img v-bind:src="availableParts.arms[0].src" title="right arm"/>
+        <button v-on:click="" class="prev-selector">&#9650;</button>
+        <button v-on:click="" class="next-selector">&#9660;</button>
       </div>
     </div>
     <div class="bottom-row">
       <div class="bottom part">
-        <img src="./images/base-single-wheel.png" title="left arm"/>
-        <button class="prev-selector">&#9668;</button>
-        <button class="next-selector">&#9658;</button>
+        <img v-bind:src="availableParts.bases[0].src" title="base"/>
+        <button v-on:click="" class="prev-selector">&#9668;</button>
+        <button v-on:click="" class="next-selector">&#9658;</button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import availableParts from '../data/parts';
+
 export default {
   name: 'RobotBuilder',
+  data() {
+    return {
+      availableParts,
+    };
+  },
 };
 </script>
 
