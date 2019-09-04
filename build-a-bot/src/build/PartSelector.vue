0a2afab2c535c9d118028173075abd0f8e6ebb23
David Greer <greerdavid4411@gmail.com>
2019-09-04 12:40:14 -0700

    Working with route params

--- build-a-bot/src/build/PartSelector.vue
+++ build-a-bot/src/build/PartSelector.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="part" :class="position">
-    <img @click="showPartInfo()" :src="selectedPart.src" title="Robot Part"/>
+    <router-link :to="{
+      name: 'Parts',
+      params: {
+        id: this.selectedPart.id,
+        partType: this.selectedPart.type,
+      }}">
+      <img :src="selectedPart.src" title="Robot Part"/>
+    </router-link>
     <button @click="selectPreviousPart()" class="prev-selector"></button>
     <button @click="selectNextPart()" class="next-selector"></button>
     <span class="sale" v-show="selectedPart.onSale">Sale!</span>
@@ -44,9 +51,6 @@ export default {
     this.emitSelectedPart();
   },
   methods: {
-    showPartInfo() {
-      this.$router.push('/parts');
-    },
     emitSelectedPart() {
       this.$emit('partSelected', this.selectedPart);
     },
